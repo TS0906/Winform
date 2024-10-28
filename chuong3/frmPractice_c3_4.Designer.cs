0115738d7a0849e3e218728cb77ddfd234dbfcc9
TS0906 <luonghuuthinh377@gmail.com>
2024-10-28 19:17:02 +0700

    update

--- chuong3/frmPractice_c3_4.Designer.cs
+++ chuong3/frmPractice_c3_4.Designer.cs
@@ -28,12 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNhapN = new System.Windows.Forms.TextBox();
+            this.txtKiemtra = new System.Windows.Forms.TextBox();
+            this.txtTim = new System.Windows.Forms.TextBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(242, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TÌM SỐ NGUYÊN TỐ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(69, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kiểm tra SNT";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(69, 300);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(165, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tìm SNT nhỏ hơn n";
+            // 
+            // txtNhapN
+            // 
+            this.txtNhapN.Location = new System.Drawing.Point(342, 154);
+            this.txtNhapN.Name = "txtNhapN";
+            this.txtNhapN.Size = new System.Drawing.Size(194, 22);
+            this.txtNhapN.TabIndex = 4;
+            this.txtNhapN.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtNhapN_KeyDown);
+            // 
+            // txtKiemtra
+            // 
+            this.txtKiemtra.Location = new System.Drawing.Point(342, 227);
+            this.txtKiemtra.Name = "txtKiemtra";
+            this.txtKiemtra.Size = new System.Drawing.Size(315, 22);
+            this.txtKiemtra.TabIndex = 5;
+            // 
+            // txtTim
+            // 
+            this.txtTim.Location = new System.Drawing.Point(342, 299);
+            this.txtTim.Name = "txtTim";
+            this.txtTim.Size = new System.Drawing.Size(315, 22);
+            this.txtTim.TabIndex = 6;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(568, 149);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(89, 32);
+            this.btnThoat.TabIndex = 7;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // frmPractice_c3_4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.txtTim);
+            this.Controls.Add(this.txtKiemtra);
+            this.Controls.Add(this.txtNhapN);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmPractice_c3_4";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmPractice_c3_4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNhapN;
+        private System.Windows.Forms.TextBox txtKiemtra;
+        private System.Windows.Forms.TextBox txtTim;
+        private System.Windows.Forms.Button btnThoat;
     }
 }
--- chuong3/frmPractice_c3_4.cs
+++ chuong3/frmPractice_c3_4.cs
@@ -15,6 +15,55 @@ namespace chuong3
         public frmPractice_c3_4()
         {
             InitializeComponent();
+            txtKiemtra.ReadOnly = true;
+            txtKiemtra.TabStop = false;
+            txtTim.ReadOnly = true;
+            txtTim.TabStop = false;
+        }
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult hoi = MessageBox.Show("Bạn có muốn thoát không?", "Thoát", MessageBoxButtons.YesNo);
+            if (hoi == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+        private void txtNhapN_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (int.TryParse(txtNhapN.Text, out int n))
+                {
+                    bool isPrime = IsPrime(n);
+                    txtKiemtra.Text = isPrime ? "Là số nguyên tố" : "Không phải là số nguyên tố";
+                    txtTim.Text = FindPrimesLessThanN(n);
+                }
+                else
+                {
+                    MessageBox.Show("Vui lòng nhập một số nguyên hợp lệ.");
+                }
+                // Ngăn không cho âm thanh mặc định khi nhấn Enter
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+        private bool IsPrime(int number)
+        {
+            if (number < 2) return false;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+        private string FindPrimesLessThanN(int n)
+        {
+            var primes = new List<int>();
+            for (int i = 2; i < n; i++)
+            {
+                if (IsPrime(i)) primes.Add(i);
+            }
+            return string.Join(", ", primes);
         }
     }
 }
